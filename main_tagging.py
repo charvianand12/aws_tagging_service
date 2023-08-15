47db42f14e5f41affe818c5ecad77c76d8e1d085
Akshay Chopra <akshaychopra5207@gmail.com>
2023-08-16 01:28:21 +0400

    working with root Organization and its units

--- main_tagging.py
+++ main_tagging.py
@@ -6,10 +6,102 @@ from validations import validate_tag_key, validate_tag_value
 load_dotenv()
 
 
+def process_account_tags(master_account_id, accounts, session):
+    # Iterate through each account
+    for account in accounts:
+        account_id = account["Id"]
+        role_name = "admin_role"  # replace "admin_role" with your role_name
+        print("\n\nProcessing account: ", account_id, "\n")
+        if len(accounts_id) == 0 or (len(accounts_id) > 0 and (account_id in accounts_id)):
+            if account_id != master_account_id:
+                access_key, secret_key, session_token = get_aws_credentials(
+                    account_id, role_name
+                )
+
+            # Iterate through each region
+            for region in regions:
+                print("  Processing region: ", region)
+                if account_id != master_account_id:
+                    client = session.client(
+                        "resourcegroupstaggingapi",
+                        region_name=region,
+                        aws_access_key_id=access_key,
+                        aws_secret_access_key=secret_key,
+                        aws_session_token=session_token,
+                    )
+                else:
+                    client = session.client("resourcegroupstaggingapi", region_name=region)
+
+                response = client.get_resources(
+                            # PaginationToken='',
+                            TagFilters=[],
+                            ResourcesPerPage=1,
+                            IncludeComplianceDetails=True,
+                            ExcludeCompliantResources=False,
+                        )
+                resources_list=response["ResourceTagMappingList"]
+                if len(resources_list) > 0:
+                    pagination_token=response["PaginationToken"]
+                    while pagination_token != "":
+                        response = client.get_resources(
+                            PaginationToken=pagination_token,
+                            TagFilters=[],
+                            ResourcesPerPage=100,
+                            IncludeComplianceDetails=True,
+                            ExcludeCompliantResources=False,
+                        )
+                        pagination_token=response["PaginationToken"]
+                        resources_list = resources_list + (response["ResourceTagMappingList"])
+                    for resource in resources_list:
+                        resource_arn = resource["ResourceARN"]
+                        resource_tags = resource["Tags"]
+                        for resource_tag in resource_tags:
+                            tag_key = resource_tag["Key"]
+                            tag_value = resource_tag["Value"]
+                            correct_key = validate_tag_key(tag_key)
+                            correct_value = validate_tag_value(tag_value, correct_key)
+                            new_tags = {correct_key: correct_value}
+                            
+                            if (tag_key != correct_key) or (tag_value != correct_value):
+                                response = client.untag_resources(
+                                    ResourceARNList=[resource_arn],
+                                    TagKeys=[
+                                        tag_key,
+                                    ],
+                                )
+                                response = client.tag_resources(
+                                    ResourceARNList=[resource_arn], Tags=new_tags
+                                )
+                                print("\n    Resource ARN:",resource_arn)
+                                print("\n    Old Key:",tag_key,"     Old Value:",tag_value, "\n    New Tag:",new_tags,"\n")
+                                print("    tag modified\n\n")
+
+
 accounts_id = []
 regions = ["us-west-2", "us-west-1", "us-east-2", "us-east-1"]
 session = boto3.Session(region_name="us-east-2")
 organizations_client = session.client("organizations")
+
+# to be added
+organization_parent_id = ''
+response = organizations_client.list_organizational_units_for_parent(ParentId=organization_parent_id)
+organizations = response['OrganizationalUnits']
+
+if len(organizations) > 0:
+    for organization in organizations:
+        ou_id = organization['Id']
+        print(f"Processing Organization: {ou_id}")
+
+        response = organizations_client.list_accounts_for_parent(ParentId=ou_id)
+        accounts = response['Accounts']
+
+        # Describe the OU to get its details
+        ou_response = organizations_client.describe_organizational_unit(OrganizationalUnitId=ou_id)
+
+        # Extract the master account ID from the response
+        master_account_id = ou_response['OrganizationalUnit']['MasterAccountId']
+        process_account_tags(master_account_id, accounts, session)
+
 response = organizations_client.describe_organization()
 organization_details = response["Organization"]
 
@@ -17,73 +109,6 @@ organization_details = response["Organization"]
 response = organizations_client.list_accounts()
 accounts = response["Accounts"]
 master_account_id = organization_details["MasterAccountId"]
+print(f"Processing Organization: {organization_details['Id']}")
+process_account_tags(master_account_id, accounts, session)
 
-
-# Iterate through each account
-for account in accounts:
-    account_id = account["Id"]
-    role_name = "admin_role"  # replace "admin_role" with your role_name
-    print("\n\nProcessing account: ", account_id, "\n")
-    if len(accounts_id) == 0 or (len(accounts_id) > 0 and (account_id in accounts_id)):
-        if account_id != master_account_id:
-            access_key, secret_key, session_token = get_aws_credentials(
-                account_id, role_name
-            )
-
-        # Iterate through each region
-        for region in regions:
-            print("  Processing region: ", region)
-            if account_id != master_account_id:
-                client = session.client(
-                    "resourcegroupstaggingapi",
-                    region_name=region,
-                    aws_access_key_id=access_key,
-                    aws_secret_access_key=secret_key,
-                    aws_session_token=session_token,
-                )
-            else:
-                client = session.client("resourcegroupstaggingapi", region_name=region)
-
-            response = client.get_resources(
-                        # PaginationToken='',
-                        TagFilters=[],
-                        ResourcesPerPage=1,
-                        IncludeComplianceDetails=True,
-                        ExcludeCompliantResources=False,
-                    )
-            resources_list=response["ResourceTagMappingList"]
-            if len(resources_list) > 0:
-                pagination_token=response["PaginationToken"]
-                while pagination_token != "":
-                    response = client.get_resources(
-                        PaginationToken=pagination_token,
-                        TagFilters=[],
-                        ResourcesPerPage=100,
-                        IncludeComplianceDetails=True,
-                        ExcludeCompliantResources=False,
-                    )
-                    pagination_token=response["PaginationToken"]
-                    resources_list = resources_list + (response["ResourceTagMappingList"])
-                for resource in resources_list:
-                    resource_arn = resource["ResourceARN"]
-                    resource_tags = resource["Tags"]
-                    for resource_tag in resource_tags:
-                        tag_key = resource_tag["Key"]
-                        tag_value = resource_tag["Value"]
-                        correct_key = validate_tag_key(tag_key)
-                        correct_value = validate_tag_value(tag_value, correct_key)
-                        new_tags = {correct_key: correct_value}
-                        
-                        if (tag_key != correct_key) or (tag_value != correct_value):
-                            response = client.untag_resources(
-                                ResourceARNList=[resource_arn],
-                                TagKeys=[
-                                    tag_key,
-                                ],
-                            )
-                            response = client.tag_resources(
-                                ResourceARNList=[resource_arn], Tags=new_tags
-                            )
-                            print("\n    Resource ARN:",resource_arn)
-                            print("\n    Old Key:",tag_key,"     Old Value:",tag_value, "\n    New Tag:",new_tags,"\n")
-                            print("    tag modified\n\n")
