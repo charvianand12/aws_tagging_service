58a3b4ead3dc2a8db85a72b61b8d4f741b460ce9
Charvi Anand <charvianand.04@gmail.com>
2023-08-27 01:19:24 +0530

    two phase tag enforcement

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # aws_tagging_service
+
+1. Run dry-run to create change set file tag_changes.csv by running
+    python main_tagging.py dry_run
+
+2. Audit the changes and modify if needed
+
+3. Apply the final changeset by running
+    python main_tagging.py
+
+4. Delete the tag_changes.csv before the next run
+
+
+# Note
+ Only tags with prefix trhc will be targetted for valideation and modification
--- main_tagging.py
+++ main_tagging.py
@@ -3,6 +3,8 @@ from credentials import get_aws_credentials
 from dotenv import load_dotenv
 from validations import validate_tag_key, validate_tag_value
 import logger_setup
+import csv
+import sys
 
 load_dotenv()
 
@@ -11,18 +13,69 @@ def get_logger():
     logger = logger_setup.setup_logger(log_filename)
     return logger
 
-def find_nested_value(dictionary, key):
-    if key in dictionary:
-        return dictionary[key]
-    for k, v in dictionary.items():
-        if isinstance(v, dict):
-            nested_value = find_nested_value(v, key)
-            if nested_value is not None:
-                return nested_value
-    return None
+def modify_tags():
+    # Enter Master Account id
+    role_name = "admin_role"
+    print(master_account_id)
+# Reading data from CSV file
+    file_name = "tag_changes.csv"
+    with open(file_name, mode="r") as file:
+        reader = csv.DictReader(file, delimiter=";")       
+        for row in reader:
+            org_id = row["org_id"]
+            account_id = row["account_id"]
+            region = row["region"]
+            resource_arn = row["resource_arn"]
+            old_tag_key = row["old_tag_key"]
+            new_tag_key = row["new_tag_key"]
+            old_tag_value = row["old_tag_value"]
+            new_tag_value = row["new_tag_value"]
+            new_tags = {new_tag_key: new_tag_value}
+            # org_ids.append(row["org_id"])
+            # account_ids.append(row["account_id"])
+            # resource_arns.append(row["resource_arn"])
+            # old_tag_keys.append(row["old_tag_key"])
+            # new_tag_keys.append(row["new_tag_key"])
+            # old_tag_values.append(row["old_tag_value"])
+            # new_tag_values.append(row["new_tag_value"])
+            if account_id != master_account_id:
+                access_key, secret_key, session_token = get_aws_credentials(
+                    account_id, role_name
+                )
+                client = session.client(
+                    "resourcegroupstaggingapi",
+                    region_name=region,
+                    aws_access_key_id=access_key,
+                    aws_secret_access_key=secret_key,
+                    aws_session_token=session_token,
+                )
+            else:
+                client = session.client("resourcegroupstaggingapi", region_name=region)
+            response = client.untag_resources(
+                ResourceARNList=[resource_arn],
+                TagKeys=[
+                    old_tag_key,
+                ],
+            )
+            response = client.tag_resources(
+                ResourceARNList=[resource_arn], Tags=new_tags
+            )    
+            logger.info("\n Old Key: %s  Old Value: %s \n  New Tag: %s",old_tag_key,old_tag_value,new_tags)
+            logger.info("    tag modified\n\n")
 
 
-def process_account_tags(master_account_id, accounts, session):
+def append_data_csv_file(tag_list):
+    # Specify the file name
+    file_name = "tag_changes.csv"
+
+    # Writing data to CSV file
+    with open(file_name, mode="a", newline="") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerows(tag_list)
+    print(f"CSV file '{file_name}' update successfully.")
+    
+    
+def process_account_tags(master_account_id, accounts, ou_id, tags_list, session):
     # Iterate through each account
     for account in accounts:
         account_id = account["Id"]
@@ -72,46 +125,27 @@ def process_account_tags(master_account_id, accounts, session):
                     resource_arn_list = []
                     for resource in resources_list:
                         resource_arn = resource["ResourceARN"]
-                        if len(resource_arn_list) == 0 or (len(resource_arn_list) > 0 and (resource_arn in resource_arn_list)):
-                            resource_tags = resource["Tags"]
-                            logger.info("\n Processing Resource: %s \n",resource_arn)
-                            for resource_tag in resource_tags:
-                                tag_key = resource_tag["Key"]
-                                tag_value = resource_tag["Value"]
+                        resource_tags = resource["Tags"]
+                        logger.info("\n Processing Resource: %s \n",resource_arn)
+                        for resource_tag in resource_tags:
+                            tag_key = resource_tag["Key"]
+                            tag_value = resource_tag["Value"]
+                            if tag_key.startswith("trhc"):
                                 correct_key = validate_tag_key(tag_key, logger)
                                 correct_value = validate_tag_value(tag_value, correct_key, logger)
                                 new_tags = {correct_key: correct_value}
-                                
                                 if (tag_key != correct_key) or (tag_value != correct_value):
-                                    try:
-                                        response = client.untag_resources(
-                                            ResourceARNList=[resource_arn],
-                                            TagKeys=[
-                                                tag_key,
-                                            ],
-                                        )
-                                    except Exception as exc:
-                                        logger.info("Not able to untag Resource: %s", resource_arn)
-                                    status_code = find_nested_value(response,'StatusCode')
-                                    logger.info("status code is %s",status_code)
-                                    if status_code != 200 and status_code is not None:
-                                        logger.error("Not able to untag Resource: %s", resource_arn)
-                                    try:
-                                        response = client.tag_resources(
-                                            ResourceARNList=[resource_arn], Tags=new_tags
-                                        )
-                                    except Exception as exc:
-                                        logger.info("Not able to tag Resource: %s", resource_arn)
-                                    status_code = find_nested_value(response,'StatusCode')
-                                    if status_code != 200 and status_code is not None:
-                                        logger.error("Not able to tag Resource: %s", resource_arn)
-                                    else:
-                                        logger.info("\n Old Key: %s  Old Value: %s \n  New Tag: %s",tag_key,tag_value,new_tags)
-                                        logger.info("    tag modified\n\n")
-
-                                    
-                                    
-
+                                    tag_data = []
+                                    tag_data.append(ou_id)
+                                    tag_data.append(account_id)
+                                    tag_data.append(region)
+                                    tag_data.append(resource_arn)
+                                    tag_data.append(tag_key)
+                                    tag_data.append(correct_key)
+                                    tag_data.append(tag_value)
+                                    tag_data.append(correct_value)
+                                    tags_list.append(tag_data)                        
+    return tags_list
 
 accounts_id = []
 regions = ["us-west-2", "us-west-1", "us-east-2", "us-east-1"]
@@ -124,7 +158,19 @@ response = organizations_client.list_organizational_units_for_parent(ParentId=or
 organizations = response['OrganizationalUnits']
 logger = get_logger()
 
-
+file_name = "tag_changes.csv"
+header_list = [
+    ["org_id", "account_id", "region", "resource_arn", "old_tag_key", "new_tag_key", "old_tag_value", "new_tag_value"],
+]
+dry_run = sys.argv[1] if len(sys.argv) > 1 else None
+# Writing data to CSV file
+if dry_run is not None and dry_run == "dry_run":
+    with open(file_name, mode="a", newline="") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerows(header_list)    
+    
+tags_list = []
+master_account_id = "473670996314"
 if len(organizations) > 0:
     for organization in organizations:
         ou_id = organization['Id']
@@ -136,9 +182,9 @@ if len(organizations) > 0:
         # Describe the OU to get its details
         ou_response = organizations_client.describe_organizational_unit(OrganizationalUnitId=ou_id)
 
-        # Extract the master account ID
-        master_account_id = ""
-        process_account_tags(master_account_id, accounts, session)
+        # Extract the master account ID from the response
+        if dry_run is not None and dry_run == "dry_run":
+            tags_list = process_account_tags(master_account_id, accounts, ou_id, tags_list, session)
 
 response = organizations_client.describe_organization()
 organization_details = response["Organization"]
@@ -146,8 +192,10 @@ organization_details = response["Organization"]
 # Retrieve accounts in your AWS organization
 response = organizations_client.list_accounts()
 accounts = response["Accounts"]
-# Extract the master account ID
-master_account_id = ""
 logger.info("Processing Organization %s", organization_details['Id'])
-process_account_tags(master_account_id, accounts, session)
-
+if dry_run is not None and dry_run == "dry_run":
+    tags_list = process_account_tags(master_account_id, accounts, organization_details['Id'], tags_list, session)
+    append_data_csv_file(tags_list)
+if dry_run is None and dry_run != "dry_run":
+    print(dry_run)
+    modify_tags()
